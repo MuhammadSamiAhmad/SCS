feca09636b299fde7d9d2fb8df388bc1a3c2d153
MuhammadSamiAhmad <mrmidosami@yahoo.com>
2024-06-28 14:11:33 +0300

    Update Footer.js

--- salesforce/src/components/Footer.js
+++ salesforce/src/components/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-blue-600 text-white py-4">
+    <footer className="bg-black text-white py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>
+          &copy; Copyrights 2024 Sahara Cloud Solutions. All rights reserved.
+        </p>
       </div>
     </footer>
   );
